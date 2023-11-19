89def9fbb177030cc5b2b021699c0b212b2e54da
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-11-19 11:29:06 +0530

    Update vignere_Cipher_code.py

--- vignere_Cipher_code.py
+++ vignere_Cipher_code.py
@@ -1,5 +1,4 @@
-# Python code to implement
-# Vigenere Cipher
+# Python code to implement Vigenere Cipher
 
 # This function generates the key in a cyclic manner until it's length isn't equal to the length of original text
 def generateKey(string, key):
